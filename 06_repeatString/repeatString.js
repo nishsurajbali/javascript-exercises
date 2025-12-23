0f9077f4640300983ee3ebd475c9e09ce9f39e6f
Nish <171086356+nishsurajbali@users.noreply.github.com>
2025-12-23 11:56:47 +0100

    Finished RepeatedString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(s, x) {
+    let repeatedString = [];
+    if (x > 0) {
+        for (i = x; i > 0; i--){
+            repeatedString += s;
+        }
+    } else if (x < 0) {
+        repeatedString += "ERROR"
+    } else {
+        repeatedString += "";
+    }
 
+    return repeatedString;
 };
+
+console.log(repeatString("test", 4))
 
 // Do not edit below this line
 module.exports = repeatString;
