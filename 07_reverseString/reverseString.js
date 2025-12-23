caef3b13a58e9435685a36c3813d696efcf376dd
Nish <171086356+nishsurajbali@users.noreply.github.com>
2025-12-23 12:06:54 +0100

    Finished reverseString javascript-exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let newString = ""
+    for (i = s.length - 1 ; i>=0; i--){
+        newString += s[i]
+    }
+    return newString;
 };
+
+console.log(reverseString("Hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
