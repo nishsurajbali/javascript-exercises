273f108598426019f3b81a35a0d7886bdef78684
Nish <171086356+nishsurajbali@users.noreply.github.com>
2025-12-29 13:25:17 +0100

    Finished the removeFromArray exercrise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter(
+        value =>
+        !args.includes(value)
+    )
 };
+
+console.log(removeFromArray([1,2,3,4], "2", 4))
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
